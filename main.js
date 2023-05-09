7e0344a722ec04cdb7ca9c6e2ae431b05f6b2356
Richard <richardzehungho@gmail.com>
2023-05-09 15:10:10 -0700

    Create initial layout and form with some js modules

---
+++ main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";const t=new class{constructor(t,e,i,n){this.title=t,this.description=e,this.dueDate=i,this.priority=n}}("chore","clean things","1998","high");(()=>{const e=document.querySelector(".mainContent");e.innerHTML="",e.append(Object.values(t))})()})();
